2c5c730b104344a2c3bc4b77f00683846b03026f
Dario Irribarra <irribarraster@gmail.com>
2020-11-12 13:32:35 -0300

    Se añaden el servidor asincrono y repositorio directo a noVNC

--- Bash_files/chromeos-browser.bash
+++ Bash_files/chromeos-browser.bash
@@ -8,8 +8,8 @@ echo "dato chromium cambiado"
 
 #~ echo "cache borrado"
 #Chromium
-chromium %U --user-agent="Mozilla/5.0 (X11; CrOS armv7l 12371.89.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36" --start-maximized --new-window $1 ||
-chromium-browser %U --user-agent="Mozilla/5.0 (X11; CrOS armv7l 12371.89.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36" --start-maximized --new-window $1
+chromium-browser %U --user-agent="Mozilla/5.0 (X11; CrOS armv7l 12371.89.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36" --start-maximized --new-window $1 ||
+chromium %U --user-agent="Mozilla/5.0 (X11; CrOS armv7l 12371.89.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36" --start-maximized --new-window $1
 
 #Chromium antiguo
 #chromium %U --user-agent="Mozilla/5.0 (X11; CrOS armv7l 11895.95.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.125 Safari/537.36" $1 || chromium-browser %U --user-agent="Mozilla/5.0 (X11; CrOS armv7l 11895.95.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.125 Safari/537.36" $1
